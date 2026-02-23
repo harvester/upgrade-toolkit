b69debdcd499602393fac49c2756727ed9800970
Zespre Chang <zespre.chang@suse.com>
2026-02-23 10:56:33 +0800

    chore: fix devcontainer setting
    Signed-off-by: Zespre Chang <zespre.chang@suse.com>

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -x
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
-chmod +x ./kind
-mv ./kind /usr/local/bin/kind
+ARCH=$(go env GOARCH)
 
-curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/linux/amd64
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/latest/kind-linux-${ARCH}"
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+curl -L -o kubebuilder "https://go.kubebuilder.io/dl/latest/linux/${ARCH}"
 chmod +x kubebuilder
-mv kubebuilder /usr/local/bin/
+sudo mv kubebuilder /usr/local/bin/
 
 KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-curl -LO "https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/${ARCH}/kubectl"
 chmod +x kubectl
-mv kubectl /usr/local/bin/kubectl
+sudo mv kubectl /usr/local/bin/kubectl
 
 docker network create -d=bridge --subnet=172.19.0.0/24 kind
 
